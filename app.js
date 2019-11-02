298a14561f2447cb00d379517965e6987aeefb15
Olgu <olgukocoglu@gmail.com>
2019-11-02 20:56:10 +0300

    Error Handling Refactor

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('./models/RecordModel');
 
-const createError = require('http-errors');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -29,18 +28,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function(req, res) {
+  res.status(404).send({ code: -404, msg: "Not found" });
 });
 
 // error handler
 app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500).send({ code: -500, msg: "Internal Server Error: " + err.message });
 });
 
 module.exports = app;
